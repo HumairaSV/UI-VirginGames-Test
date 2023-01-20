e55717fb9d9455ac2a67d5f291058833fc1e0d2c
HumairaSV <114434025+HumairaSV@users.noreply.github.com>
2023-01-20 16:00:01 +0000

    Update GamePage.java

--- src/main/java/com/virgingames/pages/GamePage.java
+++ src/main/java/com/virgingames/pages/GamePage.java
@@ -1,1 +1,23 @@
+package com.virgingames.pages;
+
+import com.aventstack.extentreports.Status;
+import com.virgingames.customlisteners.CustomListeners;
+import com.virgingames.utility.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
+
+public class GamePage extends Utility {
+
+    @CacheLookup
+    @FindBy(xpath = "//body[1]/div[2]/div[1]/div[1]/section[4]/div[1]/h1[1]" )
+    WebElement pageName;
+
+    public String verifyPageName(){
+        Reporter.log("Verify page name" + pageName.toString());
+        CustomListeners.test.log(Status.PASS, "Verify page name");
+        return getTextFromElement(pageName);
+    }
+}
 
